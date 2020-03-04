984d8e5a981fc4b01c55b4ab429fc55aaed40728
Karthik <karthik@gmail.com>
2020-03-04 12:36:22 +0530

    this is done

---
+++ typecasting.py
@@ -0,0 +1,45 @@
+#giving integer and performing typecasting
+print("giving integer and performing typecasting")
+a=100
+print(int(a))
+print(float(a))
+print(str(a))
+print(bool(a))
+print(complex(a))
+print(bytes(a))
+#giving float and performing typecasting
+print("giving float and performing typecasting")
+b=12.034
+print(int(b))
+print(float(b))
+print(str(b))
+print(bool(b))
+print(complex(b))
+#giving string and performing typecasting
+print("giving string and performing typecasting")
+c="abcde"
+print(str(c))
+print(bool(c))
+#giving boolean and performing typecasting
+print("giving boolean and performing typecasting")
+d=True
+print(int(d))
+print(float(d))
+print(str(d))
+print(bool(d))
+print(complex(d))
+print(bytes(d))
+e=False
+print(int(e))
+print(float(e))
+print(str(e))
+print(bool(e))
+print(complex(e))
+print(bytes(e))
+#giving complex and performing typecasting
+print("giving complex and performing typecasting")
+f=1+0j
+print(str(f))
+print(bool(f))
+print(complex(f))
+
