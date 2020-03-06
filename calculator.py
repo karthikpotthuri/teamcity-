cfc4348cf89d491e7bf015dd0d46a57573c6cd08
karthikpotthuri <60749516+karthikpotthuri@users.noreply.github.com>
2020-03-06 12:20:01 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+a=24
+b=4
+def add(a,b):
+	return a+b
+def sub(a,b):
+	return a-b
+def mul(a,b):
+	return a*b
+def div(a,b):
+	return a/b
+print("The sum of",a,"and",b,"is",add(a,b))
+print("The difference {} and {} is {}".format(a,b,sub(a,b)))
+print("The product {} and {} is {}".format(a,b,mul(a,b)))
+print("The division of {} by {} is {}".format(a,b,div(a,b)))
+print("Have a Nice Day :)")
